cd033c7a976af437b3e7537ab657c74682078fc1
Jerrod Junker <unsafepond@gmail.com>
2015-10-16 09:42:19 -0600

    added cheat functionality and some methods n junk

--- Documents/Turing/3module/classwork/mastermind/mastermind.rb
+++ Documents/Turing/3module/classwork/mastermind/mastermind.rb
@@ -5,10 +5,10 @@ class Mastermind
 
 	def hello
 		puts "Welcome to MASTERMIND"
-		prompt
+		starting_prompt
 	end
 
-	def prompt
+	def starting_prompt
 		puts "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
 		input = gets.chomp
 		placer(input)
@@ -22,7 +22,7 @@ class Mastermind
 		elsif input == "q"
 			quit
 		else
-			prompt
+			starting_prompt
 		end
 	end
 
@@ -36,8 +36,55 @@ class Mastermind
 	def quit
 		70.times { puts "GOODBYE!"}
 	end
-	
+
 	def play
+		@sequence = generate_beginner
+
+		puts "I have generated a beginner sequence with four elements made up of:"
+		puts "(r)ed, (g)reen, (b)lue, and (y)ellow."
+ 		puts "Use (q)uit at any time to end the game."
+		play_prompt
+
+	end
+
+	def play_prompt
+		puts "What's your guess?"
+
+		input = gets.chomp
+		input = parse_input(input)
+
+		if correct?(@sequence, input)
+			puts "Congratulations! You guessed the sequence #{@sequence.join} in X guesses over  X time."
+		else
+			puts "nah breh"
+		end
+	end
+
+	def cheat
+		p "The sequence is: #{@sequence.join}"
+		play_prompt
+	end
+
+	def generate_beginner
+		poss_chars = ["r", "g", "b", "y"]
+		sequence = []
+		4.times { sequence << poss_chars.shuffle!.sample }
+		return sequence 
+	end
+
+	def parse_input(input)
+		input = input.downcase
+		if input == "c"
+			cheat
+		elsif input == "q"
+			quit
+		else
+			input.chars
+		end
+	end
+
+	def correct?(sequence, input)
+		input == sequence
 	end
 
 
